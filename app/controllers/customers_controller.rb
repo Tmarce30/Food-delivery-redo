1648be363fecf7643d5e975f51bef7f143c27af0
Thibaud Marcé <thibaud.marce@gmail.com>
2018-01-23 18:27:39 +0100

    created add method and implemented the view to ask for customers info to add it

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -12,4 +12,10 @@ class CustomersController
     customers = @customer_repository.all
     @view.display(customers)
   end
+
+  def add
+    attributes = @view.ask_user_for_name_and_address
+    customer = Customer.new(attributes)
+    @customer_repository.add(customer)
+  end
 end
--- app/views/view_customer.rb
+++ app/views/view_customer.rb
@@ -4,4 +4,15 @@ class ViewCustomer
       puts "#{customer.id} - #{customer.name} - #{customer.address}"
     end
   end
+
+  def ask_user_for_name_and_address
+    attributes = {}
+    puts "What is the name of the customer ?"
+    puts "> "
+    attributes[:name] = gets.chomp
+    puts "What is his address ?"
+    puts "> "
+    attributes[:address] = gets.chomp
+    return attributes
+  end
 end
