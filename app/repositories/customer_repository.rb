b105218e811cabe483bdac48ea9a41f4b39a551b
Thibaud Marcé <thibaud.marce@gmail.com>
2018-01-23 18:12:05 +0100

    created add method to add a customer to the repo and implemented save_csv method to save it in the csv file

--- app/repositories/customer_repository.rb
+++ app/repositories/customer_repository.rb
@@ -1,3 +1,6 @@
+require 'csv'
+require_relative '../models/customer.rb'
+
 class CustomerRepository
   def initialize(csv_file)
     @customers = []
@@ -10,7 +13,23 @@ class CustomerRepository
     @customers
   end
 
+  def add(customer)
+    customer.id = @next_id
+    @customers << customer
+    @next_id += 1
+    save_csv
+  end
+
   private
+
+  def save_csv
+    CSV.open(@csv_file, 'w') do |csv|
+      csv << ["id", "name", "address"]
+      @customers.each do |customer|
+        csv << [customer.id, customer.name, customer.address]
+      end
+    end
+  end
 
   def load_csv
     csv_options = { headers: :first_row, header_converters: :symbol }
