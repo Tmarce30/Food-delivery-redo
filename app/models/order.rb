f39414d76b1e64b9e068283c42d0822f9c5896b6
Thibaud Marcé <thibaud.marce@gmail.com>
2018-01-24 15:45:43 +0100

    added meal amployee and customer to Order initialize method

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,13 @@
 class Order
   attr_accessor :id, :delivered
+  attr_reader :employee, :meal, :customer
 
   def initialize(attributes = {})
     @id = attributes[:id]
     @delivered = attributes[:delivered] || false
+    @meal = attributes[:meal]
+    @employee = attributes[:employee]
+    @customer = attributes[:customer]
   end
 
   def delivered?
