c18f22e3f1c2f2c292d059f45446ef648dc99b5c
Thibaud Marcé <thibaud.marce@gmail.com>
2018-01-26 12:07:28 +0100

    called all the controllers in the app file

--- app.rb
+++ app.rb
@@ -2,3 +2,7 @@ require_relative 'app/repositories/customer_repository.rb'
 require_relative 'app/repositories/employee_repository.rb'
 require_relative 'app/repositories/meal_repository.rb'
 require_relative 'app/repositories/order_repository.rb'
+
+require_relative 'app/controllers/customers_controller.rb'
+require_relative 'app/controllers/meals_controller.rb'
+require_relative 'app/controllers/orders_controller.rb'
