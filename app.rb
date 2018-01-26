8ee364497bdb038d92ad98241a82dbdbadc09aa2
Thibaud Marcé <thibaud.marce@gmail.com>
2018-01-26 12:14:22 +0100

    created csv files for every repositories to run the app and stored them in variables in app file

--- app.rb
+++ app.rb
@@ -6,3 +6,10 @@ require_relative 'app/repositories/order_repository.rb'
 require_relative 'app/controllers/customers_controller.rb'
 require_relative 'app/controllers/meals_controller.rb'
 require_relative 'app/controllers/orders_controller.rb'
+
+customer_csv_file = File.join(__dir__, 'data/customers.csv')
+employee_csv_file = File.join(__dir__, 'data/employees.csv')
+meal_csv_file = File.join(__dir__, 'data/meals.csv')
+order_csv_file = File.join(__dir__, 'data/orders.csv')
+
+
