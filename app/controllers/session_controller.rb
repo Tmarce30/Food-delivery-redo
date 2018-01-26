50870c7375a2c22a0720111254ffff2319af53fb
Thibaud Marcé <thibaud.marce@gmail.com>
2018-01-26 21:15:39 +0100

    created viewSession class thet inherit from parent base view class

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,3 +1,5 @@
+require_relative '../views/view_session.rb'
+
 class SessionController
   def initialize
     @view = ViewSession.new
---
+++ app/views/view_session.rb
@@ -0,0 +1,4 @@
+require_relative '../views/base_view.rb'
+
+class ViewSession < BaseView
+end
