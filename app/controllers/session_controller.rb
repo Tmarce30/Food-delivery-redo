11874ee42fb8ea659eaa22d253fc60927179d925
Thibaud Marcé <thibaud.marce@gmail.com>
2018-01-29 12:23:47 +0100

    created sign_in method in session controller and stored username and password asked in the view

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,4 +4,9 @@ class SessionController
   def initialize
     @view = ViewSession.new
   end
+
+  def sign_in
+    username = @view.ask_user_for_input('Username', is_num = false)
+    password = @view.ask_user_for_input('Password', is_num = false)
+  end
 end
